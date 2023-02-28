239521e402c58ffe2764aa19f57c12a594bd2970
Gabriel Cabral <gabrielcabralgc10@gmail.com>
2023-02-27 21:13:08 -0300

    fase final

--- src/controllers/criptoController.js
+++ src/controllers/criptoController.js
@@ -13,7 +13,7 @@ exports.obterMoedas = (req, res) => {
 }
 
 exports.obterMoedaPeloId = (req, res) => {
-    let id = parseInt(req.query.id);
+    let id = parseInt(req.query.id);                      // como aqui é query.id lá tem que ser id. se fosse. if teria q user if. No back usamos o query, ai tem o "?" no postman
     let moeda = service.obterMoedaPeloIdService(id)
     res.status(201).send(moeda);
 }
